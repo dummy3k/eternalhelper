8147dea687d3f34f9566e1e0ccdcf2d52e7eedd8
dummy3k <l4711@gmx.net>
2011-07-31 15:37:43 +0200

    it is kind of working

--- dijkstra.py
+++ dijkstra.py
@@ -15,35 +15,51 @@ def add_edge(cost, nodeA, nodeB):
     nodeA.edges.append((cost, nodeB))
     nodeB.edges.append((cost, nodeA))
 
-start = Node("start")
-alpha = Node("alpha")
-beta = Node("beta")
+def solve(unvisited):
+    while len(unvisited):
+        # find min cost node
+        unvisited = sorted(unvisited, lambda x, y: cmp(x.cost, y.cost))
+        current = unvisited.pop(0)
+        print "current: %s, unvisited: %s" % (current, unvisited)
+        for item in current.edges:
+            if item[1] in unvisited and item[1].cost > current.cost + item[0]:
+                item[1].cost = current.cost + item[0]
+                item[1].predecessor = current
 
-start.cost = 0
-add_edge(4, start, alpha)
-add_edge(2, start, beta)
-add_edge(1, alpha, beta)
+def get_route(nodes, dest):
+    retval = []
+    current = dest
+    while current:
+        #~ print current
+        retval.append(current)
+        current = current.predecessor
 
-unvisited = [alpha, beta, start]
+    retval.reverse()
+    return retval
 
-print "Start"
-pprint(unvisited)
+if __name__ == '__main__':
+    start = Node("start")
+    alpha = Node("alpha")
+    beta = Node("beta")
 
-while len(unvisited):
-    # find min cost node
-    unvisited = sorted(unvisited, lambda x, y: cmp(x.cost, y.cost))
-    current = unvisited.pop(0)
-    print "current: %s, unvisited: %s" % (current, unvisited)
-    for item in current.edges:
-        if item[1] in unvisited and item[1].cost > current.cost + item[0]:
-            item[1].cost = current.cost + item[0]
-            item[1].predecessor = current
+    start.cost = 0
+    add_edge(4, start, alpha)
+    add_edge(2, start, beta)
+    add_edge(1, alpha, beta)
 
-print "start: %s" % start
-print "alpha: %s" % alpha
-print "beta: %s" % beta
+    unvisited = [alpha, beta, start]
+    pprint(unvisited)
 
-current = alpha
-while current:
-    print current
-    current = current.predecessor
+    solve(unvisited)
+
+    print "start: %s" % start
+    print "alpha: %s" % alpha
+    print "beta: %s" % beta
+
+    #~ current = alpha
+    #~ while current:
+        #~ print current
+        #~ current = current.predecessor
+
+    for item in get_route(unvisited, alpha):
+        print item
